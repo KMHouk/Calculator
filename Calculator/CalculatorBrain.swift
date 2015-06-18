7280a93ca0d913ee58b3aab4b417d0a2f0b83668
Kevin Houk <kmhouk@gmail.com>
2015-06-18 16:35:33 -0400

    initial commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Kevin on 6/17/15.
+//  Copyright (c) 2015 Kevin. All rights reserved.
+//
+
+import Foundation
---
+++ Calculator/OldViewController.swift
@@ -0,0 +1,9 @@
+//
+//  OldViewController.swift
+//  Calculator
+//
+//  Created by Kevin on 6/18/15.
+//  Copyright (c) 2015 Kevin. All rights reserved.
+//
+
+import Foundation
